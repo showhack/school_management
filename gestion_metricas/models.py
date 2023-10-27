233982d71d6bdca2bfb286d26e05664ba06757ad
Alberto Licea <albertolicea00@icloud.com>
2023-10-27 10:22:07 -0400

    modelo de la metrica Distribucion de Acentos

--- gestion_metricas/models.py
+++ gestion_metricas/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+# ----------------- DISTRIBUCION DE ACENTOS -----------------
+class DistribucionAcentosCategoria(models.Model):
+    """
+    Modelo para Categorías de Distribución de Acentos.
+    """
+    nombre = models.CharField(max_length=100)
+
+class DistribucionAcentosSubCategoria(models.Model):
+    """
+    Modelo para Subcategorías de Distribución de Acentos.
+    """
+    nombre = models.CharField(max_length=100)
+    categoria = models.ForeignKey(DistribucionAcentosCategoria, on_delete=models.CASCADE)
+
+    # Relaciones con modelos que aun no existen
+    # grupo_etario = models.ManyToManyField('gestion_personas.GrupoEtario')
+    # profesor = models.ManyToManyField('gestion_personas.Profesor')
+    # instructor = models.ManyToManyField('gestion_personas.Instructor')
+
+class DistribucionAcentosPrioridades(models.Model):
+    """
+    Modelo para Prioridades de Distribución de Acentos.
+    """
+    nombre = models.CharField(max_length=100)
+    subcategorias = models.ManyToManyField(DistribucionAcentosSubCategoria, 
+                through='DistribucionAcentosRelacionPrioridad', related_name='prioridades')
+
+class DistribucionAcentosRelacionPrioridad(models.Model):
+    """
+    Modelo para la Relación entre Subcategorías y Prioridades de Distribución de Acentos.
+    """
+    subcategoria = models.ForeignKey(DistribucionAcentosSubCategoria, on_delete=models.CASCADE)
+    prioridad = models.ForeignKey(DistribucionAcentosPrioridades, on_delete=models.CASCADE)
+    mes = models.CharField(max_length=50)

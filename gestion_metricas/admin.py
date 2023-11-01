275228f00d9dc968e656cb09df913c24fc0e3b6d
leballagas <leballagas@quental.com>
2023-11-01 02:34:50 +0200

    feat(apps): add Django Admin for New Models
    1. Created admin classes in the "admin.py" file to enable Django's admin interface for the following models:
       - `DistribucionAcentosCategoria`
       - `DistribucionAcentosSubCategoria`
       - `DistribucionAcentosPrioridades`
       - `DistribucionAcentosRelacionPrioridad`
       - `DistribucionVolSemNXContenidoCualidad`
       - `DistribucionVolSemNXContenidoDistribucion`
       - `DistribucionVolSemNXRelacion`
    2. Registered these models in the Django admin site for easy management and access via the admin panel.
    
    These changes enable administrators to easily interact with and manage the new models through the Django admin interface.

--- gestion_metricas/admin.py
+++ gestion_metricas/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import (DistribucionAcentosCategoria, DistribucionAcentosSubCategoria,
+                    DistribucionAcentosPrioridades, DistribucionAcentosRelacionPrioridad,
+                    DistribucionVolSemNXContenidoCualidad, DistribucionVolSemNXContenidoDistribucion,
+                    DistribucionVolSemNXRelacion)
 
-# Register your models here.
+# Registro de modelos en el panel de administración
+admin.site.register(DistribucionAcentosCategoria)
+admin.site.register(DistribucionAcentosSubCategoria)
+admin.site.register(DistribucionAcentosPrioridades)
+admin.site.register(DistribucionAcentosRelacionPrioridad)
+admin.site.register(DistribucionVolSemNXContenidoCualidad)
+admin.site.register(DistribucionVolSemNXContenidoDistribucion)
+admin.site.register(DistribucionVolSemNXRelacion)
--- gestion_miembros/admin.py
+++ gestion_miembros/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import GrupoEtario, Pais, Provincia, Municipio, Direccion, Persona, Atleta, Entrenador, Instructor, RelacionEntrenadorGrupoEtario
 
-# Register your models here.
+# Registro de modelos en el panel de administración
+admin.site.register(GrupoEtario)
+admin.site.register(Pais)
+admin.site.register(Provincia)
+admin.site.register(Municipio)
+admin.site.register(Direccion)
+admin.site.register(Persona)
+admin.site.register(Atleta)
+admin.site.register(Entrenador)
+admin.site.register(Instructor)
+admin.site.register(RelacionEntrenadorGrupoEtario)
